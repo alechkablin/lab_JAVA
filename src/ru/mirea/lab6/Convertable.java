d4c84bcd4853b6f12bb4dbcbc67be5af8caacf04
alechka <vartanayn.alina@gmail.com>
2023-10-26 18:12:01 +0300

    check

---
+++ src/ru/mirea/lab6/Convertable.java
@@ -0,0 +1,36 @@
+package ru.mirea.lab6;
+
+interface Convertable {
+    void convert();
+
+
+    class ToCels implements Convertable {
+        private final Double kel;
+
+        public ToCels(Double kel) {
+            this.kel = kel;
+        }
+
+        @Override
+        public void convert() {
+            System.out.printf("%.2f Кельвинов = %.2f градусов Цельсия\n", kel, kel - 273.15);
+        }
+    }
+
+    class ToKel implements Convertable {
+        private final Double cels;
+
+        public ToKel(Double cels) {
+            this.cels = cels;
+        }
+
+        @Override
+        public void convert() {
+            System.out.printf("%.2f градусов Цельсия = %.2f Кельвинов\n", cels, cels + 273.15);
+        }
+    }
+
+}
+
+
+
---
+++ src/ru/mirea/lab6/Tester.java
@@ -0,0 +1,11 @@
+package ru.mirea.lab6;
+
+public class Tester {
+    public static void main(String[] args) {
+        for (int i = -100; i < 100; i = i + 10) {
+            Double d = Double.valueOf(i);
+            Convertable.ToCels cels = new Convertable.ToCels(d);
+            cels.convert();
+        }
+    }
+}
