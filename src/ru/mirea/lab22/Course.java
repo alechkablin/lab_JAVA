2e0ab5fe9717d5e89904b44aa8f5f2ec33dc9f12
alechka <vartanayn.alina@gmail.com>
2023-11-30 20:50:27 +0300

    check

---
+++ src/ru/mirea/lab22/Course.java
@@ -0,0 +1,32 @@
+package ru.mirea.lab22;
+
+
+public class Course {
+    private String CourseName;
+    private String CourseId;
+    private String CourseCategory;
+
+    public String getName() {
+        return CourseName;
+    }
+
+    public void setCourseName(String courseName) {
+        CourseName = courseName;
+    }
+
+    public String getId() {
+        return CourseId;
+    }
+
+    public void setCourseId(String courseId) {
+        CourseId = courseId;
+    }
+
+    public String getCategory() {
+        return CourseCategory;
+    }
+
+    public void setCourseCategory(String courseCategory) {
+        CourseCategory = courseCategory;
+    }
+}
---
+++ src/ru/mirea/lab22/View.java
@@ -0,0 +1,8 @@
+package ru.mirea.lab22;
+
+
+public interface View {
+    void printResult(String result);
+
+    void printException(Exception e);
+}
